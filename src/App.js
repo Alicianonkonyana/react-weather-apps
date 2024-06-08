f70bcbe37ca4f948409386bf2fc2d5e3dad17287
Alicianonkonyana <133217174+Alicianonkonyana@users.noreply.github.com>
2024-06-08 21:53:43 +0200

    Added a footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="App">
+      Hello world
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://github.com/Alicianonkonyana"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Alicia Nonkonyana
+        </a>{" "}
+        is available on{" "}
+        <a
+          href="https://github.com/Alicianonkonyana/react-weather-apps"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://monumental-griffin-c83eff.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          open-sourced on Netlify
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
