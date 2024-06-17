52a70a05f7f62d1885da5e4ba4dc9dc4929ffe66
Alicianonkonyana <133217174+Alicianonkonyana@users.noreply.github.com>
2024-06-17 11:44:52 +0200

    displayed forecast for five days

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import "./Forecast.css";
 import ForecastDay from "./ForecastDay";
@@ -11,14 +11,23 @@ export default function Forecast(props) {
     setLoaded(true);
     console.log(response.data);
   }
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
 
   if (loaded) {
     return (
       <div className="Forecast">
         <div className="row">
-          <div className="col">
-            <ForecastDay data={forecast[0]} />;
-          </div>
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <ForecastDay data={dailyForecast} />;
+                </div>
+              );
+            }
+          })}
         </div>
       </div>
     );
@@ -26,7 +35,7 @@ export default function Forecast(props) {
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
     let apiKey = "c6f8ef4575250284954db9f4dfa7a996";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
 
